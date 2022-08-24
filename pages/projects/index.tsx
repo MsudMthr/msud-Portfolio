cc9ce5518f1446fa88fb5d064844640bee4434ab
masoud <masoudmotahari1380@gmail.com>
2022-08-24 11:47:44 -0700

    install canvas library

---
+++ pages/projects/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Canvas from "src/components/Canvas";
+
+const Projects = () => {
+  return (
+    <div>
+      <Canvas>
+        <p>salam man masoud hastam</p>
+      </Canvas>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ src/components/Canvas.tsx
@@ -0,0 +1,100 @@
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import { ReactNode, useCallback } from "react";
+
+const Canvas = ({ children }: JSX.Element) => {
+  const particlesInit = useCallback(async (engine: any) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: any) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#0d4e30",
+          },
+        },
+        fpsLimit: 80,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#c11b21",
+          },
+          links: {
+            color: "#0c9220",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    >
+      ssss
+    </Particles>
+  );
+};
+
+export default Canvas;
