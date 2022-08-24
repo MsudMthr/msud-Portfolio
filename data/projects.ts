5e3103a91dbd3687b8c8d1f0386c22094d980a3e
masoud <masoudmotahari1380@gmail.com>
2022-08-24 04:18:37 -0700

    add apollo to project

--- data/projects.ts
+++ data/projects.ts
@@ -3,7 +3,6 @@ import exchange from "../src/assets/exchange.png";
 import admin from "../src/assets/admin.png";
 import musicPlayer from "../src/assets/music player.png";
 import store from "../src/assets/store.jpg";
-import tailwindTrain from "../src/assets/tailwind train.png";
 import weather from "../src/assets/weather.png";
 
 export const projectsData = [
@@ -27,10 +26,7 @@ export const projectsData = [
     url: weather,
     name: "Weather",
   },
-  {
-    url: tailwindTrain,
-    name: "Tailwind",
-  },
+
   {
     url: musicPlayer,
     name: "Music player",
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-[#1F282F] min-h-screen">
-      <Component {...pageProps} />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="min-h-screen bg-[#1F282F]">
+        <Component {...pageProps} />
+      </div>
+    </ApolloProvider>
   );
 }
 
